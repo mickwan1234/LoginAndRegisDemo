86ffa5582d129bc47fc8cbca73332285a4955e9f
nghĩa nguyễn hiếu <mickwan1234@gmail.com>
2021-02-20 18:31:11 +0700

    first commit

---
+++ src/test/java/com/example/LoginAndRegisDemo/LoginAndRegisDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LoginAndRegisDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAndRegisDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
